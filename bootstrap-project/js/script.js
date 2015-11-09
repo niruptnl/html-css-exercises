744bfdadd231261525578f4366a5e6ce59755d64
unknown <NA324237@L-777726102.wipro.com>
2015-11-09 11:26:14 +0530

    sample website added

---
+++ bootstrap-project/js/script.js
@@ -0,0 +1,38 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+$(window).load(function(){
+  $('#myModal1').modal({display:'show',backdrop:'static',keyboard:false});
+});
+
+$(document).ready(function(){
+
+  $('#myModal1').on('hidden.bs.modal', function () {
+      $("#myModal1 iframe").attr("src", $("#myModal1 iframe").attr("src"));
+  });
+
+    $('#alertMe').click(function(e){
+      e.preventDefault();
+      $('#successAlert').slideDown();
+    });
+
+    $('.carousel').carousel({
+   interval: 3000
+ });
+
+ $('a.pop').click(function(e){
+   e.preventDefault();
+ });
+
+ $('a.pop').popover();
+
+ $('#tooltip').tooltip({html:"true"});
+
+  });
